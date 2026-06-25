3e1b474e45671fda50ee933238419096a7595132
Mark Chan <mchan.lg@gmail.com>
2026-06-25 08:24:07 +0800

    Refresh market Yearly Trend price policy

--- market.php
+++ market.php
@@ -2036,7 +2036,7 @@ function adsBlocked(callback){
 <script src="assets/js/TA.js"></script>
 <script src="assets/js/Highstock.js?v=20260624.3"></script>
 <script src="assets/js/stockCompare.js?v=20260623.10"></script>
-<script src="assets/js/yearlyTrendChart.js?v=20260623.10"></script>
+<script src="assets/js/yearlyTrendChart.js?v=20260625.1"></script>
 <script src="assets/js/seasonality.js?v=20260623.5"></script>
 <script>
 (function () {
