608d3efbfb7bd7656e414831fedf769fc4ecd2e1
Mark Chan <mchan.lg@gmail.com>
2026-07-09 19:14:43 +0800

    Improve chart share feedback

--- assets/js/pages/tool-share.js
+++ assets/js/pages/tool-share.js
@@ -1,13 +1,34 @@
 (function () {
   "use strict";
 
-  function notify(message) {
+  function notify(message, button) {
+    if (button) {
+      var originalText = button.getAttribute("data-original-html") || button.innerHTML;
+      button.setAttribute("data-original-html", originalText);
+      button.textContent = message;
+      window.setTimeout(function () {
+        button.innerHTML = originalText;
+      }, 1800);
+    }
+
     if (typeof window.showNotification === "function") {
       window.showNotification(message);
       return;
     }
 
-    console.log(message);
+    var notification = document.createElement("div");
+    notification.className = "notification";
+    notification.textContent = message;
+    document.body.appendChild(notification);
+    window.setTimeout(function () {
+      notification.classList.add("show");
+    }, 10);
+    window.setTimeout(function () {
+      notification.classList.remove("show");
+      window.setTimeout(function () {
+        notification.remove();
+      }, 500);
+    }, 2200);
   }
 
   function toAbsoluteUrl(url) {
@@ -22,7 +43,7 @@
     return toAbsoluteUrl(window.newRelativePathQueryTab2 || window.location.pathname + window.location.search);
   }
 
-  function fallbackCopy(url) {
+  function fallbackCopy(url, button) {
     var textarea = document.createElement("textarea");
     textarea.value = url;
     textarea.setAttribute("readonly", "");
@@ -41,26 +62,26 @@
     document.body.removeChild(textarea);
 
     if (copied) {
-      notify("Link copied");
+      notify("Link copied", button);
     } else {
-      notify("Unable to copy link");
+      notify("Unable to copy link", button);
     }
   }
 
-  function copyUrl(url) {
+  function copyUrl(url, button) {
     if (navigator.clipboard && window.isSecureContext) {
       navigator.clipboard.writeText(url).then(function () {
-        notify("Link copied");
+        notify("Link copied", button);
       }).catch(function () {
-        fallbackCopy(url);
+        fallbackCopy(url, button);
       });
       return;
     }
 
-    fallbackCopy(url);
+    fallbackCopy(url, button);
   }
 
-  function shareUrl(url, title) {
+  function shareUrl(url, title, button) {
     if (navigator.share) {
       navigator.share({
         title: title,
@@ -71,12 +92,12 @@
           return;
         }
 
-        copyUrl(url);
+        copyUrl(url, button);
       });
       return;
     }
 
-    copyUrl(url);
+    copyUrl(url, button);
   }
 
   function bindButton(id, urlFactory, title, useNativeShare) {
@@ -88,9 +109,9 @@
     button.addEventListener("click", function () {
       var url = urlFactory();
       if (useNativeShare) {
-        shareUrl(url, title);
+        shareUrl(url, title, button);
       } else {
-        copyUrl(url);
+        copyUrl(url, button);
       }
     });
   }
