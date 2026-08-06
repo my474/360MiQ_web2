e2ac09c3d99b7bf6020ecb363b3588d3b0c4c003
Mark Chan <mchan.lg@gmail.com>
2026-08-06 10:18:51 +0800

    Load base styles on account settings

--- account_settings.php
+++ account_settings.php
@@ -83,6 +83,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta property="og:title" content="Account Settings - 360MiQ.com" />
     <meta name="description" content="Manage your 360MiQ account, privacy, and workspace data." />
     <title>Account Settings - 360MiQ.com</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/css/account.css?v=20260806.3">
     <link rel="stylesheet" href="assets/css/workspace.css?v=20260804.1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
