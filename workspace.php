8137e1c7aa6daf21532b6f646bd93b85aec42d9c
Mark Chan <mchan.lg@gmail.com>
2026-08-12 21:25:22 +0800

    Improve workspace notification badge contrast

--- workspace.php
+++ workspace.php
@@ -23,7 +23,7 @@ if (!$user) {
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
     <link rel="stylesheet" href="assets/css/account.css?v=20260806.7">
-    <link rel="stylesheet" href="assets/css/workspace.css?v=20260806.3">
+    <link rel="stylesheet" href="assets/css/workspace.css?v=20260812.4">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 </head>
 <body data-community-enabled="<?php echo $community_enabled ? 'true' : 'false'; ?>">
